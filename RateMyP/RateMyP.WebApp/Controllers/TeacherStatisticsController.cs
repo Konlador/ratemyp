e3a012cae3c9595fae57c382d0919673933ffb5f
Vingis Simonas <simonas.vingis@mif.stud.vu.lt>
2019-11-11 18:07:19 +0200

    Made minor changes to TeacherStatisticsController

--- RateMyP/RateMyP.WebApp/Controllers/TeacherStatisticsController.cs
+++ RateMyP/RateMyP.WebApp/Controllers/TeacherStatisticsController.cs
@@ -21,23 +21,20 @@ namespace RateMyP.WebApp.Controllers
             m_analyzer = new TeacherStatisticsAnalyzer(m_context);
         }
         
-        [HttpGet("teacher={teacherId}/date={startDate}-{endDate}/parts={parts}")]
-        public async Task<ActionResult<TeacherStatistic>> GetTeacherStatistics(Guid teacherId, long startDate, long endDate, int parts)
+        [HttpGet("teacher={teacherId}/history/parts={parts}")]
+        public async Task<ActionResult<TeacherStatistic>> GetTeacherStatistics(Guid teacherId, int parts)
             {
-            var startDateTime = new DateTime(startDate);
-            var endDateTime = new DateTime(endDate);
-            var teacherStatisticByDate = new TeacherStatistic()
+            var teacherStatistic = new TeacherStatistic()
                 {
                 Id = Guid.NewGuid(),
                 TeacherId = teacherId,
                 AverageMark = await m_analyzer.GetTeacherAverageMark(teacherId),
-                AverageMarkByDate = await m_analyzer.GetTeacherAverageMark(teacherId, startDateTime, endDateTime),
-                AverageMarkList = await m_analyzer.GetTeacherAverageMarkList(teacherId, startDateTime, endDateTime, parts),
+                AverageMarkList = await m_analyzer.GetTeacherAverageMarkList(teacherId, parts),
                 AverageLevelOfDifficulty = await m_analyzer.GetTeachersAverageLevelOfDifficultyRating(teacherId),
-                AverageWouldTakeAgainRatio = await m_analyzer.GetTeachersWouldTakeTeacherAgainRatio(teacherId)
+                WouldTakeAgainRatio = await m_analyzer.GetTeachersWouldTakeTeacherAgainRatio(teacherId)
                 };
 
-            return teacherStatisticByDate;
+            return teacherStatistic;
             }
         }
 }
