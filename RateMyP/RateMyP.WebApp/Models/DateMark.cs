f26198027ee3eba838e4f312bb4bcf8fe9d632f7
Vingis Simonas <simonas.vingis@mif.stud.vu.lt>
2019-11-11 18:07:58 +0200

    Added a DateMark object that stores the date and mark of a rating

---
+++ RateMyP/RateMyP.WebApp/Models/DateMark.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RateMyP.WebApp.Models
+    {
+    public class DateMark : IEquatable<TeacherStatistic>
+        {
+        public DateTime Date { get; set; }
+        public double Mark { get; set; }
+
+        public bool Equals([AllowNull] TeacherStatistic other)
+            {
+            throw new NotImplementedException();
+            }
+        }
+    }
