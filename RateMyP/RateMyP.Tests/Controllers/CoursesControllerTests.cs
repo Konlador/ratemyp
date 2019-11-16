64218e074405211cc35ed91cd9962014512c4242
Vismantas Stonkus <Vismantas.Stonkus@gmail.com>
2019-11-17 00:41:58 +0200

    Tests

---
+++ RateMyP/RateMyP.Tests/Controllers/CoursesControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using RateMyP.WebApp;
+using RateMyP.WebApp.Controllers;
+using RateMyP.WebApp.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace RateMyP.Tests.Controllers
+    {
+    public class CoursesControllerTests : TestsBase
+        {
+        private ICoursesController m_controller;
+
+        [SetUp]
+        public new void SetUp()
+            {
+            Seed(Context);
+            m_controller = new CoursesController(Context);
+            }
+
+        [Test]
+        public async Task GetCourses_ReturnsAllCourses()
+            {
+            var courses = await m_controller.GetCourses();
+            }
+
+        private static void Seed(RateMyPDbContext context)
+            {
+            context.Courses.Add(new Course
+                {
+                Id = Guid.NewGuid(),
+                Name = "Komparchas",
+                Credits = 5,
+                Faculty = "MIF",
+                CourseType = CourseType.BUS
+                });
+
+            context.Courses.Add(new Course
+                {
+                Id = Guid.NewGuid(),
+                Name = "Duomenu strukturos",
+                Credits = 10,
+                Faculty = "MIF",
+                CourseType = CourseType.Complimentary
+                });
+
+            context.SaveChanges();
+            }
+        }
+    }
---
+++ RateMyP/RateMyP.WebApp/Models/Badge.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RateMyP.WebApp.Models
+    {
+    public class Badge
+        {
+        public Guid Id { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        }
+    }
